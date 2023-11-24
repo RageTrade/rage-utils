28151a67c8e767b6822f7c53b84e5e7dd0e03afe
protodev-rage <protodev@rage.trade>
2023-11-24 13:36:27 +0530

    docs: add inline documentation for examples

--- examples/read.ts
+++ examples/read.ts
@@ -2,13 +2,23 @@ import { init, createAccount, setupExecute } from '../src'
 import GmxV2Service from 'perp-aggregator-sdk/src/exchanges/gmxv2'
 
 async function main() {
+  // takes .env and initializes provider, signer and bundler
   const env = init()
+
+  // create gmx v2 instance to use for read and write operations
   const gmxV2 = new GmxV2Service()
 
+  // creates instance of smart account
+  // deployment of smart account is lazy and handled internally
+  // i.e it will deploy bytecode in when first transaction is made
+  // createAccount must be called to get smart account instance and its address to use for read & write fn
   const account = await createAccount(env.signer, env.bundler)
+
+  // get account address to use in exchange
   const address = await account.getAccountAddress()
 
   // get all positions
+  // refer sdk for in-depth documentation
   const position = await gmxV2.getAllPositions(address, undefined)
   console.dir(position, { depth: 4 })
 }
